de5eddd09d43c792029e16b165b47186b0c6cfd0
Alcrane <alcrane@wanadoo.fr>
2015-10-18 18:26:51 +0200

    Finalisation de la sécurité
    securite-et-gestion-des-utilisateurs

--- UserBundle/OCUserBundle.php
+++ UserBundle/OCUserBundle.php
@@ -1,9 +1,17 @@
 <?php
 
+//-- OC/UserBundle/OCUserBundle.php
+
 namespace OC\UserBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class OCUserBundle extends Bundle
 {
+
+    public function getParent()
+    {
+        return 'FOSUserBundle';
+    }
+
 }
